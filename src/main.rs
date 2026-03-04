ebfbaf09bd69c51abefcafd766b38b9b4f68bfa0
Cauce <pcaucev@gmail.com>
2026-03-04 13:35:38 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 #[allow(unused_imports)]
-use std::io::{self, Write};
+use std::io::stdin;
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
+    let mut command = String::new();
+
     print!("$ ");
+    stdin().read_line(&mut command).unwrap();
+
+    println!("{}: command not found", command.trim());
+
     io::stdout().flush().unwrap();
 }
